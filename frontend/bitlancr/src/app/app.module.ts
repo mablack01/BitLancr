8bb1cf6cfb0525f8f1d7b72718b06bd08985a4c6
Miles <mblack@platformq.com>
2019-03-12 18:54:55 -0400

    Generate a navigation bar.

--- frontend/bitlancr/src/app/app.module.ts
+++ frontend/bitlancr/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './global/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
