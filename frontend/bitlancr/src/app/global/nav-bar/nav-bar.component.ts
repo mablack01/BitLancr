880d71326d47821b8e2922f3b9e14855fa9376ce
Miles <mblack@platformq.com>
2019-03-12 21:04:54 -0400

    Forgot to push the navigation bar.

---
+++ frontend/bitlancr/src/app/global/nav-bar/nav-bar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  isMobile: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.adjustScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.adjustScreenSize();
+  }
+
+  adjustScreenSize() {
+    if (window.screen.width <= 768) { // phones/tablets
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+  }
+
+}
