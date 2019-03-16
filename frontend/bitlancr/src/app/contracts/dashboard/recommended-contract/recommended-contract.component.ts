0c06c064ca6c4cdbddc4f2c02cc72eecfddc3f13
Miles <mblack@platformq.com>
2019-03-16 18:20:52 -0400

    Added row and column rendering for x recommendations.

--- frontend/bitlancr/src/app/contracts/dashboard/recommended-contract/recommended-contract.component.ts
+++ frontend/bitlancr/src/app/contracts/dashboard/recommended-contract/recommended-contract.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecommendedContractComponent implements OnInit {
 
-	constructor() { }
+	mockData = [];
+	contracts = [];
+
+	constructor() {
+		this.mockData[0] = {
+			title: "UX Design",
+			imgUrl: "assets/oracle/logo.png",
+			skills: ["angular", "javascript", "html", "css"],
+			avgBid: 35
+		},
+			this.mockData[1] = {
+				title: "UX Design",
+				imgUrl: "assets/oracle/logo.png",
+				skills: ["angular", "javascript", "html", "css"],
+				avgBid: 35
+			},
+			this.mockData[2] = {
+				title: "UX Design",
+				imgUrl: "assets/oracle/logo.png",
+				skills: ["angular", "javascript", "html", "css"],
+				avgBid: 35
+			},
+			this.mockData[3] = {
+				title: "UX Design",
+				imgUrl: "assets/oracle/logo.png",
+				skills: ["angular", "javascript", "html", "css"],
+				avgBid: 35
+			},
+			this.mockData[4] = {
+				title: "UX Design",
+				imgUrl: "assets/oracle/logo.png",
+				skills: ["angular", "javascript", "html", "css"],
+				avgBid: 35
+			},
+			this.mockData[5] = {
+				title: "UX Design",
+				imgUrl: "assets/oracle/logo.png",
+				skills: ["angular", "javascript", "html", "css"],
+				avgBid: 35
+			}
+		this.renderColumns();
+	}
 
 	ngOnInit() {
+	}
+
+	renderColumns() {
+		for (let i = 0; i < this.mockData.length; i += 4) {
+			this.contracts.push({ data: this.mockData.slice(i, i + 4) });
+		}
 	}
 
 }
