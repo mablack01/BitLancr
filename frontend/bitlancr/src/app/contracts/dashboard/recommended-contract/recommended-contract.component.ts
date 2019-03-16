8e56839884c06f6b07c9ddb6d9f1296543e974be
Miles <mblack@platformq.com>
2019-03-16 19:34:08 -0400

    Rendering of skill logos.

--- frontend/bitlancr/src/app/contracts/dashboard/recommended-contract/recommended-contract.component.ts
+++ frontend/bitlancr/src/app/contracts/dashboard/recommended-contract/recommended-contract.component.ts
@@ -14,38 +14,38 @@ export class RecommendedContractComponent implements OnInit {
 		this.mockData[0] = {
 			title: "UX Design",
 			imgUrl: "assets/oracle/logo.png",
-			skills: ["angular", "javascript", "html", "css"],
-			avgBid: 35
+			skills: ["angular", "bootstrap"],
+			avgBid: 3500
 		},
 			this.mockData[1] = {
-				title: "UX Design",
-				imgUrl: "assets/oracle/logo.png",
-				skills: ["angular", "javascript", "html", "css"],
-				avgBid: 35
+				title: "Webapp Design",
+				imgUrl: "assets/companies/company1.png",
+				skills: ["react", "material"],
+				avgBid: 420
 			},
 			this.mockData[2] = {
-				title: "UX Design",
-				imgUrl: "assets/oracle/logo.png",
-				skills: ["angular", "javascript", "html", "css"],
-				avgBid: 35
+				title: "Simple Application",
+				imgUrl: "assets/intel/job1.png",
+				skills: ["java"],
+				avgBid: 69
 			},
 			this.mockData[3] = {
-				title: "UX Design",
-				imgUrl: "assets/oracle/logo.png",
-				skills: ["angular", "javascript", "html", "css"],
-				avgBid: 35
+				title: "Grid Layout",
+				imgUrl: "assets/companies/company3.png",
+				skills: ["html"],
+				avgBid: 710
 			},
 			this.mockData[4] = {
-				title: "UX Design",
-				imgUrl: "assets/oracle/logo.png",
-				skills: ["angular", "javascript", "html", "css"],
-				avgBid: 35
+				title: "Endpoint Creation",
+				imgUrl: "assets/companies/company5.png",
+				skills: ["flask"],
+				avgBid: 21
 			},
 			this.mockData[5] = {
-				title: "UX Design",
-				imgUrl: "assets/oracle/logo.png",
-				skills: ["angular", "javascript", "html", "css"],
-				avgBid: 35
+				title: "Database Modeling",
+				imgUrl: "assets/companies/company4.png",
+				skills: ["sql"],
+				avgBid: 12345
 			}
 		this.renderColumns();
 	}
@@ -53,10 +53,15 @@ export class RecommendedContractComponent implements OnInit {
 	ngOnInit() {
 	}
 
+	//Maybe we should set a maximum amount of recommendations returned so the page doesn't render forever?
 	renderColumns() {
 		for (let i = 0; i < this.mockData.length; i += 4) {
 			this.contracts.push({ data: this.mockData.slice(i, i + 4) });
 		}
+	}
+
+	formatMoney(n: Number) {
+		return "$" + n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	}
 
 }
