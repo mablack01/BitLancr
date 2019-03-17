e3e3ff7f2103250d90a79285fede409966a4e95a
Paul Mikulskis <mikulskisp@gmail.com>
2019-03-17 19:57:30 -0400

    postgresql

--- backend/python/main.py
+++ backend/python/main.py
@@ -1,5 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+POSTGRES = {
+    'user': 'pi',
+    'pw': '1234count',
+    'db': 'test',
+    'host': 'bitlancr.ddns.net',
+    'port': '5432',
+}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route("/")
 def hello():
