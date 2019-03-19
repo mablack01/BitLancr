bf0c09153505dc34bb8c8b1c0e84902667315990
Miles <mblack@platformq.com>
2019-03-19 12:26:43 -0400

    Added routing for the profile.

--- frontend/bitlancr/src/app/app-routing.module.ts
+++ frontend/bitlancr/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
 	{ path: '', component: DashboardComponent },
 	{ path: 'dashboard', component: DashboardComponent },
+	{ path: 'dash', component: DashboardComponent },
+	{ path: 'home', component: DashboardComponent },
+	{ path: 'profile', component: ProfileComponent },
+	{ path: 'profile/:handle', component: ProfileComponent },
 	{ path: '**', component: DashboardComponent } //later we can add a 404
 ];
 
--- frontend/bitlancr/src/app/profile/profile.component.ts
+++ frontend/bitlancr/src/app/profile/profile.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'profile',
 	templateUrl: './profile.component.html',
-	styleUrls: ['./profile.component.css']
+	styleUrls: ['./profile.component.css', './../app.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-	constructor() { }
+	loaded = false;
+	handle: String;
+	userData: [];
+
+	constructor(private route: ActivatedRoute) { }
 
 	ngOnInit() {
+		this.handle = this.route.snapshot.paramMap.get('handle');
+		if (this.handle != null) {
+			//This code simulates we got a handle, load the user.
+		} else {
+			//This code simulates no handle was provided, show logged in user.
+		}
+		//Gather user data from endpoint using handle, if exists.
+		//Simulating an end point here.
+		var root = this;
+		setTimeout(function() {
+			root.loaded = true;
+			console.log("Data loaded.");
+		}, 3000);
 	}
 
 }
